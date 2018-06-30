2b86c3c53dd2abe4a7ffde538f5ee982221fbc15
Richard Wright <richard.o.wright@gmail.com>
2018-06-30 15:53:50 -0400

    fix for check on content existing

--- contentScript.js
+++ contentScript.js
@@ -2,12 +2,14 @@ if (window.location.href.indexOf("bizjournals.com") > -1) {
   const hiddenStory = document.getElementsByClassName(
     "js-pre-chunks__story-body"
   );
-  if (hiddenStory) {
+  if (hiddenStory && hiddenStory.length>0) {
     hiddenStory[0].style.display = "block";
   }
 
-  const payWallMessage = document.getElementsByClassName("chunk chunk--flex@lg chunk--paywall");
-  if (hiddenStory) {
+  const payWallMessage = document.getElementsByClassName(
+    "chunk chunk--flex@lg chunk--paywall"
+  );
+  if (payWallMessage && payWallMessage.length>0 ) {
     payWallMessage[0].style.display = "none";
   }
 }
