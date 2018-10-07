c2ffc2253f90366b1f123c706749347b30a06859
Elad Katz <katzelad@gmail.com>
2018-10-07 03:53:49 +0300

    fix haaretz.co.il

--- background.js
+++ background.js
@@ -96,7 +96,8 @@ function setDefaultOptions() {
 
 const blockedRegexes = [
 /.+:\/\/.+\.tribdss\.com\//,
-/thenation\.com\/.+\/paywall-script\.php/
+/thenation\.com\/.+\/paywall-script\.php/,
+/haaretz\.co\.il\/htz\/js\/inter\.js/
 ];
 
 var enabledSites = [];
