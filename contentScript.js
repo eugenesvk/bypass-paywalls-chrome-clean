7a943491810c5cf3f5bfc42d2736d84fec5b5ae4
magnolia1234 <7676006-magnolia1234@users.noreply.gitlab.com>
2020-04-29 20:10:51 +0200

    Fix-update Challenges.fr

--- contentScript.js
+++ contentScript.js
@@ -466,6 +466,10 @@ else if (matchDomain('ladepeche.fr')) {
 
 else if (matchDomain('challenges.fr')) {
     document.addEventListener('DOMContentLoaded', () => {
+        const hidden_par = document.querySelector('.corps[style="display:none"]');
+        if (hidden_par) {
+            hidden_par.removeAttribute('style');
+        }
         const hidden_image = document.querySelectorAll('img.lazyload');
         for (let i = 0; i < hidden_image.length; i++) {
             var src = hidden_image[i].src;
